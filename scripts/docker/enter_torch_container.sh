5a2a9280ee924b4a6f759881dad2e9576b9a6629
meinie <ziyuan>
2026-05-10 15:52:27 +0800

    feat: add active learning framework skeleton

--- scripts/docker/enter_torch_container.sh
+++ scripts/docker/enter_torch_container.sh
@@ -4,6 +4,8 @@ cd /data/zft
 
 docker run --rm -it \
   --gpus all \
+  --user $(id -u):$(id -g) \
+  -e PYTHONDONTWRITEBYTECODE=1 \
   -v /data/zft:/data/zft \
   -w /data/zft \
   cuda-torch:cuda11.3-cudnn8-ubuntu18.04-torch2.4 \
